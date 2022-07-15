e1065486cb44eaaf1154f56d87e64b0aa050e4b2
Nyaka <dammnyaka@gmail.com>
2022-07-15 16:44:15 +0300

    k

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <div  className="player">
-        <Container/>
+        <Container db={db}/>
         <Media db={db}/>
       </div>
     </div>
